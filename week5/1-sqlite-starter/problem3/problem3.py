692298de06cabc19cffcae416cd2139ce74081ef
dsspasov <dsspasov94@gmail.com>
2014-11-11 14:41:41 +0200

    a new commit

--- week5/1-sqlite-starter/problem3/problem3.py
+++ week5/1-sqlite-starter/problem3/problem3.py
@@ -5,7 +5,7 @@ def main():
     conn = sqlite3.connect("polyglot.db")
     cursor = conn.cursor()
 
-    result = cursor.execute("SELECT id, language FROM languages")
+    result = cursor.execute("SELECT id, language, answers FROM languages")
 
     for row in result:
         print(row)
